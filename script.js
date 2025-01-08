bebf21f3e904b464c81b5a6a1317f11acd660de8
Reniuha <naglisr45@gmail.com>
2025-01-08 17:22:52 +0200

    done

--- script.js
+++ script.js
@@ -1,65 +1,80 @@
-const next = document.getElementById('next-btn');
-const back = document.getElementById('back-btn');
-const cover = document.getElementById('cover');
-const container = document.getElementById('container');
-const rickRoll = document.getElementById("RickRoll");
+// References to DOM Elements
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const book = document.querySelector("#book");
 
-let isRickRollActive = false;
+const paper1 = document.querySelector("#p1");
+const paper2 = document.querySelector("#p2");
 
-next.addEventListener("click", () => {
-    const currentZIndex = window.getComputedStyle(cover).zIndex;
-    
-    if(window.getComputedStyle(cover).backgroundImage === "url('doYou.jpg')") {
-    } else if (currentZIndex === '2') {
-        cover.style.zIndex = '-1';
-    }
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNextPage);
 
-    if(decode.textContent === "S29kYXM6IEthbMSXZG9z") {
-        window.location.href = "second.html"
-    }
-});
+let currentLocation = 1;
+let numOfPapers = 2;
+let maxLocation = numOfPapers + 1;
 
-back.addEventListener("click", () => {
-    const currentZIndex = window.getComputedStyle(cover).zIndex;
-    
-    if(currentZIndex === '-1') {
-        cover.style.zIndex = '2';
-        cover.style.backgroundImage = "url('pic.jpg')";
+function openBook() {
+    book.style.transform = "translateX(50%)";
+    prevBtn.style.transform = "translateX(-180px)";
+    nextBtn.style.transform = "translateX(180px)";
+}
+
+function closeBook(isAtBeginning) {
+    if(isAtBeginning) {
+        book.style.transform = "translateX(0%)";
     } else {
-        cover.style.backgroundImage = "url('doYou.jpg')";
+        book.style.transform = "translateX(100%)";
     }
-});
+    
+    prevBtn.style.transform = "translateX(0px)";
+    nextBtn.style.transform = "translateX(0px)";
+}
 
-rickRoll.style.cursor = "pointer";
-
-rickRoll.addEventListener("click", () => {
-    if(isRickRollActive) {
-        container.style.display = "block"; 
-        document.body.style.backgroundImage = ""; 
-        isRickRollActive = false; 
-    } else {
-        container.style.display = "none"; 
-        document.body.style.backgroundImage = "url('200w.gif')"; 
-        isRickRollActive = true; 
-
-        setTimeout(() => {
-            container.style.display = "block"; 
-            document.body.style.backgroundImage = ""; 
-            isRickRollActive = false;
-            
-            document.body.style.position = "initial"; 
-        }, 5000);
+function goNextPage() {
+    if(currentLocation < maxLocation) {
+        switch(currentLocation) {
+            case 1:
+                openBook();
+                paper1.classList.add("flipped");
+                paper1.style.zIndex = 1;
+                break;
+            case 2:
+                paper2.classList.add("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 3:
+                paper3.classList.add("flipped");
+                paper3.style.zIndex = 3;
+                closeBook(false);
+                break;
+            default:
+                throw new Error("unkown state");
+        }
+        currentLocation++;
     }
-})
+}
 
-const decode = document.getElementById("decode");
-const help = document.getElementById("help");
+function goPrevPage() {
+    if(currentLocation > 1) {
+        switch(currentLocation) {
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3;
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 4:
+                openBook();
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 1;
+                break;
+            default:
+                throw new Error("unkown state");
+        }
 
-decode.addEventListener("click", () => {
-    decode.textContent = "S29kYXM6IEthbMSXZG9z";
-    help.textContent = "Need help?";
-});
-
-help.addEventListener("click", () => {
-    help.textContent = "Naudok base64 decoder";
-});
+        currentLocation--;
+    }
+}
